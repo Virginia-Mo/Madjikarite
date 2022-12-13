b5503376da7559d6c6f6ff88681ead93bbc63c5f
Virginia Mo <antonio.vir@hotmail.com>
2022-12-13 21:59:16 +0100

    deploy

--- Client/src/middlewares/products.js
+++ Client/src/middlewares/products.js
@@ -16,17 +16,7 @@ const productsAPI = (store) => (next) => (action) => {
         .catch((error) => console.log(error))
       next(action);
       break;
-      // case GET_CATEGORIES:
-      //   console.log("click")
-      //   axios
-      //     .get(`${API_BASE_URL}/product:${slug}`)
-      //     .then((response) => {
-      //     console.log(response.data);
-      //       store.dispatch(saveSingleProduct(response.data));
-      //     })
-      //     .catch((error) => console.log(error))
-      //   next(action);
-      //   break;
+      
     default:
       next(action);
   }
