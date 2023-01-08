ef5316cd1e517b5386334e51548c33eefa62694a
CarolineKretz <caroline.kretz68@gmail.com>
2023-01-08 14:42:01 +0100

    images Process

--- Client/src/components/Process/index.js
+++ Client/src/components/Process/index.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
 
-import { GiWeight } from 'react-icons/gi';
-import { TbArrowDownLeft, TbArrowDownRight } from 'react-icons/ti';
+import step1 from 'src/assets/imgs/step1.png';
+import step2 from 'src/assets/imgs/step2.png';
+import step3 from 'src/assets/imgs/step3.png';
+import step4 from 'src/assets/imgs/step4.png';
+import step5 from 'src/assets/imgs/step5.png';
+import step6 from 'src/assets/imgs/step6.png';
+import step7 from 'src/assets/imgs/step7.png';
 
 import AOS from 'aos';
 
@@ -25,6 +30,7 @@ function Process() {
         data-aos="fade-up-right"
         data-aos-duration="1500">
         <div className="process__article--title">Un arbre</div>
+        <img className="process__article--img" src={step1} alt="step1" />
         <p className="process__article--text">Au Tchad, il n’y a pas de culture de l’arbre à karité. 
         C’est un arbre très commun dans le sud du pays, que l’on trouve en brousse, autour des villages.</p>
       </article>
@@ -37,7 +43,7 @@ function Process() {
         data-aos="fade-up-left"
         data-aos-duration="1500">
         <div className="process__article--title">Fruits ramassés</div>
-        <div className="process__article--icon"><GiWeight /></div>
+        <img className="process__article--img" src={step2} alt="step2" />
         <p className="process__article--text">Tout commence avec le ramassage des fruits qui ne sont mûrs 
         que lorsqu’ils sont tombés de l’arbre.</p>
       </article>
@@ -50,7 +56,7 @@ function Process() {
         data-aos="fade-up-right"
         data-aos-duration="1500">
         <div className="process__article--title">Fruits dépulpés</div>
-        <div className="process__article--icon"><GiWeight /></div>
+        <img className="process__article--img" src={step3} alt="step3" />
         <p className="process__article--text">L’étape suivante est le dépulpage : 
         la séparation de la chair et de la noix. La chair est comestible, très appréciée des enfants.</p>
       </article>
@@ -59,6 +65,7 @@ function Process() {
         data-aos="fade-up-left"
         data-aos-duration="1500">
         <div className="process__article--title">Noix bouillies</div>
+        <img className="process__article--img" src={step4} alt="step4" />
         <p className="process__article--text">Les noix sont ensuite bouillies avant 
         d’être pilées pour enlever la coque et récupérer les amandes.</p>
       </article>
@@ -67,7 +74,7 @@ function Process() {
         data-aos="fade-up-right"
         data-aos-duration="1500">
         <div className="process__article--title">Noix séchées</div>
-        <div className="process__article--icon"><GiWeight /></div>
+        <img className="process__article--img" src={step5} alt="step5" />
         <p className="process__article--text">Les amandes sont grillées puis broyées 
         au moulin. </p>
       </article>
@@ -76,7 +83,7 @@ function Process() {
         data-aos="fade-up-left"
         data-aos-duration="1500"> 
         <div className="process__article--title">Noix décoquées = amandes</div>
-        <div className="process__article--icon"><GiWeight /></div>
+        <img className="process__article--img" src={step6} alt="step6" />
         <p className="process__article--text">La poudre est alors mélangée à de l’eau 
         et malaxée pour obtenir une pâte blanc ivoire.</p>
       </article>
@@ -85,7 +92,7 @@ function Process() {
         data-aos="fade-up-right"
         data-aos-duration="1500"> 
         <div className="process__article--title">Beurre de karité</div>
-        <div className="process__article--icon"><GiWeight /></div>
+        <img className="process__article--img" src={step7} alt="step7" />
         <p className="process__article--text">Cette pâte est enfin plongée dans de l’eau 
         bouillante afin de séparer l’huile des impuretés. Reposée à température ambiante, 
         l’huile devient beurre de karité.</p>
